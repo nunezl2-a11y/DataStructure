a4f31c5dede114525d64d0f1d8854d8ae667d542
nunezl2-a11y <nunezl2@go.stockton.edu>
2026-02-05 18:54:57 -0500

    Add files via upload

---
+++ HolderTester.java
@@ -0,0 +1,47 @@
+
+
+public class HolderTester
+{
+    public static void main (String[] args)
+    {
+        LuannaHolder Hold = new LuannaHolder();
+        
+        Hold.addToBucket("Bucket1");
+        System.out.println("Contains: "+ Hold.getSize() + " Value");
+        Hold.addToBucket("Bucket2");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        Hold.addToBucket("Bucket3");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        Hold.addToBucket("Bucket4");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        Hold.addToBucket("Bucket5");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        Hold.addToBucket("Bucket6");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        Hold.addToBucket("Bucket7");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        Hold.addToBucket("Bucket8");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        Hold.addToBucket("Bucket9");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        Hold.addToBucket("Bucket10");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        Hold.addToBucket("Bucket11");
+        System.out.println("Contains: "+ Hold.getSize() + " Values");
+        System.out.println("");
+        
+        System.out.println("In Position 6 the Value is " + Hold.getPosition(6));
+        System.out.println("");
+        System.out.println("Values: ");
+        Hold.printAll();
+        
+        System.out.println("");
+        System.out.println("Lets remove Value 4...");
+        Hold.remove(4);
+        System.out.println("Now lets see what our array looks like:");
+        Hold.printAll();
+        
+        System.out.println("");
+        Hold.Clear();
+    }
+}
---
+++ LuannaHolder.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+
+public class LuannaHolder
+{
+    private int size;
+    private String bucket[];
+    private String bigBuck[];
+    
+    public LuannaHolder()
+    {
+        size = 0;
+        bucket = new String[10];
+        bigBuck= new String[20];
+    }
+    
+    public void addToBucket(String value)
+    {
+       if(size<10)
+       {
+           bucket[size]= value;
+       }
+       else
+       {
+           for(int i = 0; i < bucket.length; i++)
+           {
+               bigBuck[i] = bucket[i];
+           }
+           bigBuck[size]=value;
+       }
+       size++;
+    }
+    
+    public int getSize()
+    {
+        return size;
+    }
+    
+    public String getPosition(int pos)
+    {
+        if(pos< 0 || pos >= size)
+        return null;
+        if(size<=10)
+        return bucket[pos];
+        else
+        return bigBuck[pos];
+    }
+    
+    public void printAll()
+    {
+        for(int i = 0; i < bucket.length; i++)
+        {
+            if(bucket[i] != null)
+            {
+                System.out.println(getPosition(i));
+            }
+        }
+    }
+    
+    public void Clear()
+    {
+        bigBuck = null;
+        bucket = null;
+        size = 0;
+        
+        System.out.println("Clearing Array...");
+        System.out.println("Array now contains : " + size + " Values");
+    }
+}
